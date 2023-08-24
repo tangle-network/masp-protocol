5e3a7e80ab9ce8c2893c8a2149dba0950daec6df
dev0x1 <gaurav1@tutamail.com>
2023-08-24 13:25:07 +0530

    restore temp changes

--- scripts/bash/build_circuits.sh
+++ scripts/bash/build_circuits.sh
@@ -22,6 +22,82 @@ copy_to_fixtures () {
     cp artifacts/circuits/$outdir/$circuit\_js/witness_calculator.js solidity-fixtures/solidity-fixtures/$anchorType/$size/witness_calculator.cjs
 }
 
+###
+# WEBB BATCH TREE UPDATER
+###
+
+echo "Compiling batch insertion for 4 leafs (levels=2)"
+compile batch_tree_4 batchMerkleTreeUpdate_4 4
+copy_to_fixtures batch_tree_4 batchMerkleTreeUpdate_4 4 batch-tree
+
+echo "Compiling batch insertion for 8 leafs (levels=3)"
+compile batch_tree_8 batchMerkleTreeUpdate_8 8
+copy_to_fixtures batch_tree_8 batchMerkleTreeUpdate_8 8 batch-tree
+
+echo "Compiling batch insertion for 16 leafs (levels=4)"
+compile batch_tree_16 batchMerkleTreeUpdate_16 16
+copy_to_fixtures batch_tree_16 batchMerkleTreeUpdate_16 16 batch-tree
+
+echo "Compiling batch insertion for 32 leafs (levels=5)"
+compile batch_tree_32 batchMerkleTreeUpdate_32 32
+copy_to_fixtures batch_tree_32 batchMerkleTreeUpdate_32 32 batch-tree
+
+echo "Compiling batch insertion for 64 leafs (levels=6)"
+compile batch_tree_64 batchMerkleTreeUpdate_64 64
+copy_to_fixtures batch_tree_64 batchMerkleTreeUpdate_64 64 batch-tree
+
+###
+# WEBB MASP-VANCHORS
+###
+
+echo "Compiling Webb style multi-asset Poseidon vanchor 2 circuit w/ 2 inputs"
+compile masp_vanchor_2 masp_vanchor_2_2 2
+copy_to_fixtures masp_vanchor_2 masp_vanchor_2_2 2 masp_vanchor_2
+
+echo "Compiling Webb style multi-asset Poseidon vanchor 8 circuit w/ 2 inputs"
+compile masp_vanchor_2 masp_vanchor_2_8 8
+copy_to_fixtures masp_vanchor_2 masp_vanchor_2_8 8 masp_vanchor_2
+
+echo "Compiling Webb style multi-asset Poseidon vanchor 2 circuit w/ 16 inputs"
+compile masp_vanchor_16 masp_vanchor_16_2 2
+copy_to_fixtures masp_vanchor_16 masp_vanchor_16_2 2 masp_vanchor_16
+
+echo "Compiling Webb style multi-asset Poseidon vanchor 8 circuit w/ 16 inputs"
+compile masp_vanchor_16 masp_vanchor_16_8 8
+copy_to_fixtures masp_vanchor_16 masp_vanchor_16_8 8 masp_vanchor_16
+
+###
+# WEBB MASP VANCHOR FOREST
+###
+
+# echo "Compiling Webb style multi-asset vanchor forest 2 circuit w/ 2 inputs"
+# compile vanchor_forest_2 vanchor_forest_2_2 2
+# copy_to_fixtures vanchor_forest_2 vanchor_forest_2_2 2 vanchor_forest_2
+
+# echo "Compiling Webb style multi-asset vanchor forest 8 circuit w/ 2 inputs"
+# compile vanchor_forest_2 vanchor_forest_2_8 8
+# copy_to_fixtures vanchor_forest_2 vanchor_forest_2_8 8 vanchor_forest_2
+# #
+# echo "Compiling Webb style multi-asset vanchor forest 2 circuit w/ 16 inputs"
+# compile vanchor_forest_16 vanchor_forest_16_2 2
+# copy_to_fixtures vanchor_forest_16 vanchor_forest_16_2 2 vanchor_forest_16 
+# #
+# echo "Compiling Webb style multi-asset vanchor forest 8 circuit w/ 2 inputs"
+# compile vanchor_forest_16 vanchor_forest_16_8 8
+# copy_to_fixtures vanchor_forest_16 vanchor_forest_16_8 8 vanchor_forest_16 
+
+
+###
+# WEBB ANONIMITY MINING REWARD SYSTEM
+###
+
+echo "Compiling anonimity mining circuit"
+compile reward_2 reward_30_2 30
+copy_to_fixtures reward_2 reward_30_2 30 reward_2
+
+echo "Compiling anonimity mining circuit"
+compile reward_8 reward_30_8 30
+copy_to_fixtures reward_8 reward_30_8 30 reward_8
 
 # ###
 # # WEBB MASP SWAP SYSTEM
